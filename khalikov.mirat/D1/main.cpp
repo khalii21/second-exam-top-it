f9c401cb12661f817d1892b38192b52ce8aee4d1
student <student@desktop-5jmrsk4.ius.local>
2026-02-19 19:04:17 +0300

    resize impl

--- khalikov.mirat/D1/main.cpp
+++ khalikov.mirat/D1/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 
+char * Resize(char * data, size_t size, size_t capacity)
+    {
+      char * new_data = new char[capacity * 2];
+      for (size_t i = 0; i < size; i++)
+      {
+        new_data[i] = data[i];
+      }
+      delete[] data_;
+      capacity *= 2;
+    }
+
+
+
 char * createarray(int a);
 
 int main()
 {
   int b;
   char c;
+  char * res = nullptr;
   while (std::cin >> b && std::cin >> a)
   {
     char * data = createarray(b);
+
 
   }
 }
