82e74facd9bf857c257e91fa628ff4a3f33ae5a9
student <student@desktop-5jmrsk4.ius.local>
2026-02-19 18:57:11 +0300

    createarray prepared

--- khalikov.mirat/D1/main.cpp
+++ khalikov.mirat/D1/main.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
 
+char * createarray(int a);
+
 int main()
-{}
+{
+  int b;
+  char c;
+  while (std::cin >> b && std::cin >> a)
+  {
+    char * data = createarray(b);
+
+  }
+}
